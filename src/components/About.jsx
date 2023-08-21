0bdf11690a4d4dce9da6247101375cad8a2a907a
Deepak Singh Chauhan <85275831+Deepak3440@users.noreply.github.com>
2023-08-21 19:50:33 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,10 @@ const AboutUs = () => {
           <img src={Pic} alt="About Us" />
         </div>
         <div className="about-text">
-          <h1>About Us</h1>
+          <h1>About Me</h1>
           <p>
           Hello, I'm Deepak Singh Chauhan, a highly motivated individual with a passion for computer science and technology. I am currently pursuing my<b> B.Tech in Computer Science Engineering from Moradabad Institute of Technology (AKTU-Lucknow)</b>, with a batch year of 2020-2024.<p/>
-          <h2>Begin Your Learning Odyssey</h2>
+          <h2>About My Contribution</h2>
           <p>
           Apart from my academic achievements, I have actively participated in various courses and certifications to enhance my skills. I have completed courses in<b> Full Stack Development,Java,Machine Learning</b> as well as improving my soft skills and personality development. I have also acquired knowledge in the field of Data Science.
           </p>
